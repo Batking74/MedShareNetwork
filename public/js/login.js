236c27459e9615da8dbdd701ec9389be84af7cfd
Antonio Johnson <142611747+Antoniomj10@users.noreply.github.com>
2023-12-14 20:07:06 -0500

    added code to profile.handlebars and code to the login.js

---
+++ public/js/login.js
@@ -0,0 +1,34 @@
+// Attach event listeners when the document is ready
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.querySelector('.login-form');
+    const emailInput = document.getElementById('email-login');
+    const passwordInput = document.getElementById('password-login');
+
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const email = emailInput.value;
+        const password = passwordInput.value;
+
+        // Validate the inputs (e.g., check for empty fields)
+
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    window.location.href = '/dashboard';
+                } else {
+                
+                    console.error('Authentication failed');
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    });
+});
